7c0b6d7630a31f5efac842018b81b0a418b93c2a
Ruchita Kalantri <rkalantri.kalantri@yahoo.com>
2021-07-31 13:30:00 +0530

    add all packages and work on shopping cart

--- models/user.js
+++ models/user.js
@@ -31,6 +31,36 @@ const userSchema = new Schema ({
 
 });
 
+userSchema.methods.addToCart = function (product) {
+  // item already in cart
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1;
+    // copy old element first : updatedCartItems
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0 ) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1 ;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      //item does not exist before
+      updatedCartItems.push({ 
+        productId : product._id , 
+        quantity : newQuantity
+      });
+    }
+
+    // items to store into database
+    const updatedCart = {
+      items : updatedCartItems
+    };
+
+    this.cart = updatedCart;
+    return this.save();
+
+}
+
 module.exports = mongoose.model('User' , userSchema);
 
 
